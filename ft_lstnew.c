9eb77126dc1df5a0a5d585bb097e9c84536d252a
aelphias <igormitchell11@gmail.com>
2020-03-11 18:17:23 +0300

    doesn't worj

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,15 +6,56 @@
 /*   By: aelphias <aelphias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 02:46:26 by aelphias          #+#    #+#             */
-/*   Updated: 2020/02/18 03:12:49 by aelphias         ###   ########.fr       */
+/*   Updated: 2020/03/07 20:56:05 by aelphias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "stdio.h"
+
+/* What should it do?
+1) initializy elements of structure by copy
+2)allocate and return link
+ */
+
+
+/* • content : The data contained in the link. The void * allows to store any kind of
+data.
+• content_size : The size of the data stored. The void * type doesn’t allow you to know the size of the pointed data, as a consequence, it is necessary to save its size. For instance, the size of the string "42" is 3 bytes and the 32bits integer 42 has a size of 4 bytes.
+• next : The next link’s address or NULL if it’s the last link.      */
 
 t_list  *ft_lstnew(void const *content, size_t  content_size)
 {
-    t_list *data;
+    printf("{ 4 }");
+    t_list *node;
+    
+    if (!(node = (t_list *)malloc(sizeof(t_list))))
+        return (NULL);
+    printf("{ 5 }");
+
+    if (content == NULL)
+        node->content = NULL;
+    else
+        ft_memcpy(node->content, content, content_size);
+    node->next = NULL;
+    return (node);
+}
+
+int main(void)
+{
+    int a;
+    t_list *list;
+    char *str = "love is the answer";
+    
+    a = 45;
+    printf("{ 1 }\n");
+    list = ft_lstnew(&a, sizeof(int));
+    printf("{ 2 }");
+    printf("%s", list->content);
+    //printf("NULL\n");
+    return (0);
+}
+/* t_list *data;
     
     if (!(data = (t_list *)malloc(sizeof(t_list))))
         return(NULL);
@@ -29,5 +70,4 @@ t_list  *ft_lstnew(void const *content, size_t  content_size)
     data->content = (void *)content;
     data->content_size = content_size;
     data->next = NULL;
-    return (data);
-}
+    return (data); */
